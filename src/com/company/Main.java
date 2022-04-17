91cdb03afdeb7bb9fe9a5ea7d5daed2f98d8f5cd
Pradyumn <msdfanatic@gmail.com>
2022-04-17 11:31:32 +0530

    changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Dijkstra");
+	System.out.println("Dijkstra 1");
+        System.out.println("Dijkstra 2");
     }
 }
