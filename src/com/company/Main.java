796850545a2412da4602e5cec17fa0bdc77d7753
Pradyumn <msdfanatic@gmail.com>
2022-04-17 11:47:47 +0530

    version new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Dijkstra 1");
         System.out.println("Dijkstra 2");
+        System.out.println("Dijkstra 3");
     }
 }
